35008c203754fdfff170044acdf7a3ca41d2368d
Chetan Padia <chetbox@users.noreply.github.com>
2024-01-17 17:31:31 +0000

    allow hosting at subpath

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import topLevelAwait from "vite-plugin-top-level-await";
 
 export default defineConfig({
+  base: "./",
   plugins: [topLevelAwait()],
   optimizeDeps: {
     exclude: ["quickjs-emscripten"], // workaround for Vite hosting .wasm files with wrong mimetype

d2c0bbaf3847f63bd47a6ceeefce6bd77e49f490
Chetan Padia <chetbox@users.noreply.github.com>
2024-01-17 17:14:50 +0000

    top level await workaround

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
+import topLevelAwait from "vite-plugin-top-level-await";
 
 export default defineConfig({
+  plugins: [topLevelAwait()],
   optimizeDeps: {
     exclude: ["quickjs-emscripten"], // workaround for Vite hosting .wasm files with wrong mimetype
   },
